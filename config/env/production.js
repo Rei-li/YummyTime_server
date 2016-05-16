a0a69ad44ea070c2ac5fdc75f8777f74ce1b4f66
Rei-li <rei-li@mail.ru>
2016-05-16 17:47:20 +0300

    change origin

--- config/env/production.js
+++ config/env/production.js
@@ -3,12 +3,12 @@
 module.exports = {
   db: `mongodb://${process.env.YUMMY_USER}:${process.env.YUMMY_PASSWORD}@ds011382.mlab.com:11382/yummydb`,
   port: process.env.PORT || 3000,
-  originURL: 'https://yummy-time.herokuapp.com',
+  originURL: 'https://my-yummytime.herokuapp.com',
   secret: 'secret',
   google: {
     // eslint-disable-next-line max-len
     clientID: process.env.GOOGLE_CLIENTID || '1071029381615-tucu38j7kboh2kk4f8tj9br832gihl03.apps.googleusercontent.com',
     clientSecret: process.env.GOOGLE_SECRET || 'mAgQY2Z9TPJzFGEGkQjU1-37',
-    callbackURL: 'https://yummy-time.herokuapp.com'
+    callbackURL: 'https://my-yummytime.herokuapp.com'
   }
 };
