8b2716fc80566fa5b0b7ab02526d63f9a8554ad0
Rei-li <rei-li@mail.ru>
2016-05-17 00:33:02 +0300

    add email to config

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-  // db: `mongodb://${process.env.YUMMY_USER}:${process.env.YUMMY_PASSWORD}@ds011382.mlab.com:11382/yummydb`,
   db: 'mongodb://ytm-usr:ytm-pwd@ds032579.mlab.com:32579/yummytimetest',
   port: process.env.PORT || 3000,
   originURL: 'https://my-yummytime.herokuapp.com',
@@ -11,5 +10,7 @@ module.exports = {
     clientID: process.env.GOOGLE_CLIENTID || '1071029381615-tucu38j7kboh2kk4f8tj9br832gihl03.apps.googleusercontent.com',
     clientSecret: process.env.GOOGLE_SECRET || 'mAgQY2Z9TPJzFGEGkQjU1-37',
     callbackURL: 'https://my-yummytime.herokuapp.com'
-  }
+  },
+  smtp: 'smtps://yummytime.test%40gmail.com:yandex-shri-minsk-2016@smtp.gmail.com',
+  systemEmail: 'yummytime.test@gmail.com'
 };
