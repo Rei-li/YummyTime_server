2edd44c47deb4fbb31bba91b3e67ee1ddd601440
Rei-li <rei-li@mail.ru>
2016-05-24 15:13:32 +0300

    add logical deletion

--- app/models/portionProduct.js
+++ app/models/portionProduct.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 
 const portionProductSchema = new Schema({
   quantity: { type: Number, required: true },
-  product: { ref: 'Product', type: Schema.ObjectId }
+  product: { ref: 'Product', type: Schema.ObjectId },
+  deleted: { type: Boolean, required: true, default: false }
 });
 
 module.exports = mongoose.model('PortionProduct', portionProductSchema);
